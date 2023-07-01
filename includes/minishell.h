19ac0a6f00d1e91d30557ac7f0416c3123513295
Y S <yuu@Mac.local>
2023-07-01 19:16:51 +0900

    clean .h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ysakahar <ysakahar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 19:27:37 by ysakahar          #+#    #+#             */
-/*   Updated: 2023/07/01 19:12:41 by ysakahar         ###   ########.fr       */
+/*   Updated: 2023/07/01 19:16:26 by ysakahar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,21 @@ typedef enum s_ops
 	HEREDOC,
 }	t_ops;
 
+typedef struct s_state
+{
+	char			*input_args;
+	char			**paths;
+	char			**envp;
+	char			*pwd;
+	char			*old_pwd;
+	int				num_pipes;
+	int				*pid;
+	bool			is_heredoc;
+	bool			is_reset_completed;
+	t_lexer			*lexer;
+	struct s_cmd	*cmd;
+}	t_state;
+
 typedef struct s_lexer
 {
 	char			*str;
@@ -77,21 +92,6 @@ typedef struct s_parser
 	t_lexer			*redirections;
 	struct s_state	*state;
 }	t_parser;
-
-typedef struct s_state
-{
-	char			*input_args;
-	char			**paths;
-	char			**envp;
-	char			*pwd;
-	char			*old_pwd;
-	int				num_pipes;
-	int				*pid;
-	bool			is_heredoc;
-	bool			is_reset_completed;
-	t_lexer			*lexer;
-	struct s_cmd	*cmd;
-}	t_state;
 
 typedef struct s_cmd
 {
